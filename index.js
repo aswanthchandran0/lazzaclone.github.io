a601476268fec46fd9d391c0983ff74c5ab68569
aswanthchandran0 <139605642+aswanthchandran0@users.noreply.github.com>
2023-07-28 15:25:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const title = document.querySelector('#main-heading');
+
+title.style.color ='red';
+
+console.log(title);
+
+const listitems =document.querySelectorAll('.list-item');
+
+
+for(i=0;i<listitems.length;i++){
+  
+listitems[i].style.color = 'green';
+}
+
+ul.append(li)
+
+const firstlistitem =document.querySelector('.list-item');
+
+console.log(firstlistitem.innertext);
